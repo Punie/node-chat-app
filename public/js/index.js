aeb5949b2759cc5550d6614341ef4fce3c25f05e
Hugo Saracino <hugo.saracino@gmail.com>
2018-01-16 07:53:58 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Hugo',
-    text: 'Roger 5/5!'
-  });
 });
 
 socket.on('disconnect', function () {
